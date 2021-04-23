b0a9e309392ecd59cffcd9f3666e53a7ef335b99
MAD71241 <michele.deliso@gmail.com>
2021-04-23 16:46:21 +0200

    Inserisco HTML, rimuovo console.log

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,7 @@ if (isNaN(routeLength)) {
     alert("La durata del tuo itinerario deve essere espressa con una cifra. Riprova!");
 }
 console.log(routeLength);
+document.getElementById("length").innerHTML = routeLength + " chilometri.";
 
 //l'età del passeggero.
 var passengerAge = parseInt(prompt("Quanti anni ha il passeggero del viaggio?"));
@@ -11,6 +12,7 @@ if (isNaN(passengerAge)) {
     alert("L'età del passeggero deve essere espressa con una cifra. Riprova!")
 }
 console.log(passengerAge);
+document.getElementById("age").innerHTML = passengerAge + " anni.";
 
 //Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio
 /*secondo queste regole:
@@ -24,14 +26,14 @@ if (passengerAge < 18) {
     function percentage(num, per) {
         return (num / 100) * per
     }
-    console.log(discountedMessage + (totalPrice - percentage(totalPrice, 20)).toFixed(2) + " euro.");
+    document.getElementById("ticket").innerHTML = (discountedMessage + (totalPrice - percentage(totalPrice, 20)).toFixed(2) + " euro.");
 }
 //va applicato uno sconto del 40% per gli over 65. 
 else if (passengerAge > 65) {
     function percentage(num, per) {
         return (num / 100) * per
     }
-    console.log(discountedMessage + (totalPrice - percentage(totalPrice, 40)).toFixed(2) + " euro.");
+    document.getElementById("ticket").innerHTML = (discountedMessage + (totalPrice - percentage(totalPrice, 40)).toFixed(2) + " euro.");
 } else {
-    console.log(message + totalPrice + " euro.");
+    document.getElementById("ticket").innerHTML = (message + totalPrice + " euro.");
 }
